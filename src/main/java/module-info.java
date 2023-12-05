8b0dba49df8276af6fe7d0572a8eb4e8ba59071d
211218 <211218@ids.upchiapas.edu.mx>
2023-12-04 21:16:09 -0600

    Revert "fix commmet principal model"
    This reverts commit 5104e7c4f3e0646efe71dc2640233999ac0f42a2.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
-module java {
+module Restaurante {
     requires javafx.fxml;
     requires javafx.controls;
-    exports com.example.demo6.principal;
+    exports com.example.demo6;
 
     opens com.example.demo6.principal;
     opens com.example.demo6.Controllers;
